681c048c99806410609da5c747f14646e918bfb6
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2016-03-30 22:30:02 +0800

    Update update.sh

--- ForUbuntu/update.sh
+++ ForUbuntu/update.sh
@@ -6,8 +6,9 @@
      sudo apt-get update -o Acquire::No-Cache=True
      sudo apt-get update -o Acquire::BrokenProxe=True
      sudo rm -vf /var/lib/apt/lists/*     
-     sudo apt-get update     -y                                    
+     sudo apt-get update                                      
      sudo apt-get upgrade   -y
      sudo apt-get dist-upgrade  -y 
-     sudo apt autoremove -y  
+     sudo apt-get autoremove  -y   
+
 
