a5d5ecbb9954d5097d42ed18313d1960fb68e75b
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2016-10-18 20:24:48 +0800

    Create subread-build-index.sh

---
+++ BuildIndex/ChIP-seq/subread-build-index.sh
@@ -0,0 +1,5 @@
+mkdir		ce11			
+mkdir		mm10			
+					
+subread-buildindex  -o 	ce11/ce11	        Shortcuts/ce11.fa        >>ce11.buildIndex.runLog		2>&1	
+subread-buildindex  -o 	mm10/mm10		Shortcuts/mm10.fa	 >>mm10.buildIndex.runLog		2>&1						
