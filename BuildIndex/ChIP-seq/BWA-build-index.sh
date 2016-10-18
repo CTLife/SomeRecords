68a2302b8e6511ca5ca39d8d9538ec994a88d01b
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2016-10-18 20:23:38 +0800

    Create BWA-build-index.sh

---
+++ BuildIndex/ChIP-seq/BWA-build-index.sh
@@ -0,0 +1,6 @@
+mkdir		ce11			
+mkdir		mm10			
+								
+bwa  index  -p  ce11/ce11	        Shortcuts/ce11.fa        >>ce11.buildIndex.runLog		2>&1	
+bwa  index  -p  mm10/mm10		Shortcuts/mm10.fa	 >>mm10.buildIndex.runLog		2>&1						
+
