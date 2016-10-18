2282f9920682d07c17032fde0dace4efc7a66868
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2016-10-18 20:23:07 +0800

    Create bowtie2-build-index.sh

---
+++ BuildIndex/ChIP-seq/bowtie2-build-index.sh
@@ -0,0 +1,9 @@
+mkdir  ce11
+mkdir  mm10
+
+bowtie2-build  Shortcuts/ce11.fa         ce11/ce11       >>ce11.buildIndex.runLog      2>&1
+bowtie2-build  Shortcuts/mm10.fa         mm10/mm10       >>mm10.buildIndex.runLog      2>&1
+
+
+
+
