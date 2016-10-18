e857eb479673f58bb23737b390dc5f8c05a3c60b
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2016-10-18 20:25:19 +0800

    Create Yara-build-index.sh

---
+++ BuildIndex/ChIP-seq/Yara-build-index.sh
@@ -0,0 +1,6 @@
+mkdir		ce11			
+mkdir		mm10			
+					
+yara_indexer  --output-prefix	ce11/ce11	        Shortcuts/ce11.fa          >>ce11.buildIndex.runLog		2>&1	
+yara_indexer  --output-prefix	mm10/mm10		Shortcuts/mm10.fa	   >>mm10.buildIndex.runLog		2>&1						
+ 	
