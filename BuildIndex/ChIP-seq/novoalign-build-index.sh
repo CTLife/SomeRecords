788139a9c7ee8f3a8b2a9f4c37735c0ae95e59e3
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2016-10-18 20:24:15 +0800

    Create novoalign-build-index.sh

---
+++ BuildIndex/ChIP-seq/novoalign-build-index.sh
@@ -0,0 +1,7 @@
+mkdir		ce11			
+mkdir		mm10			
+									
+novoindex	  -s 1   -t 8	ce11/ce11	        Shortcuts/ce11.fa            >>ce11.buildIndex.runLog		2>&1	
+novoindex	  -s 1   -t 8	mm10/mm10		Shortcuts/mm10.fa	     >>mm10.buildIndex.runLog		2>&1						
+
+
