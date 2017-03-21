4829adf9841e5d4bf4cd721c0068d4800bb1464d
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2017-03-21 19:03:24 +0800

    Create wig_to_bigwig.sh

---
+++ shell/wig_to_bigwig.sh
@@ -0,0 +1,8 @@
+wigToBigWig   wig/H3.bgsub.Fnor.smooth.wig         mm10.chrom.sizes   bigwig/H3.bw       
+wigToBigWig   wig/week0.bgsub.Fnor.smooth.wig      mm10.chrom.sizes   bigwig/week0.bw   
+wigToBigWig   wig/week1.bgsub.Fnor.smooth.wig      mm10.chrom.sizes   bigwig/week1.bw 
+wigToBigWig   wig/week2.bgsub.Fnor.smooth.wig      mm10.chrom.sizes   bigwig/week2.bw 
+wigToBigWig   wig/week4.bgsub.Fnor.smooth.wig      mm10.chrom.sizes   bigwig/week4.bw 
+wigToBigWig   wig/week6.bgsub.Fnor.smooth.wig      mm10.chrom.sizes   bigwig/week6.bw 
+wigToBigWig   wig/week8.bgsub.Fnor.smooth.wig      mm10.chrom.sizes   bigwig/week8.bw 
+
