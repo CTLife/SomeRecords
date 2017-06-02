811baf894172ed2df2a903593bd324c4072cad97
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2017-06-02 22:41:40 +0800

    Create bbmap-build-index.sh

---
+++ BuildIndex/ChIP-seq2/bbmap-build-index.sh
@@ -0,0 +1,17 @@
+bbmap.sh      ref=Shortcuts/ce11/ce11.fasta                     k=13   path=ce11                            -Xmx64g  >>ce11.runLog 2>&1                                 
+bbmap.sh      ref=Shortcuts/danRer10/danRer10.fasta             k=13   path=danRer10                        -Xmx64g  >>danRer10.runLog 2>&1                                 
+bbmap.sh      ref=Shortcuts/dm6/dm6.fasta                       k=13   path=dm6                             -Xmx64g  >>dm6.runLog 2>&1                                 
+bbmap.sh      ref=Shortcuts/hg38/hg38.fasta                     k=13   path=hg38                            -Xmx64g  >>hg38.runLog 2>&1                                 
+bbmap.sh      ref=Shortcuts/mm10/mm10.fasta                     k=13   path=mm10                            -Xmx64g  >>mm10.runLog 2>&1                                 
+
+bbmap.sh      ref=Shortcuts/OtherGenomes/Adapters.fasta        k=13   path=OtherGenomes/Adapters          -Xmx64g  >>OtherGenomes.Adapters.runLog 2>&1                                 
+bbmap.sh      ref=Shortcuts/OtherGenomes/Ecoli.fasta           k=13   path=OtherGenomes/Ecoli             -Xmx64g  >>OtherGenomes.Ecoli.runLog 2>&1                                 
+bbmap.sh      ref=Shortcuts/OtherGenomes/phiX174.fasta         k=13   path=OtherGenomes/phiX174           -Xmx64g  >>OtherGenomes.phiX174.runLog 2>&1                                 
+bbmap.sh      ref=Shortcuts/OtherGenomes/UniVectors.fasta      k=13   path=OtherGenomes/UniVectors        -Xmx64g  >>OtherGenomes.UniVectors.runLog 2>&1                                 
+
+bbmap.sh      ref=Shortcuts/sacCer3/sacCer3.fasta               k=13   path=sacCer3                         -Xmx64g  >>sacCer3.runLog 2>&1                                 
+
+
+bbmap.sh      ref=Shortcuts/hg38/hg38.ensembl.genome.fasta                     k=13   path=hg38.ensembl                            -Xmx64g  >>hg38.ensembl.runLog 2>&1                                 
+bbmap.sh      ref=Shortcuts/mm10/mm10.ensembl.genome.fasta                     k=13   path=mm10.ensembl                            -Xmx64g  >>mm10.ensembl.runLog 2>&1   
+
