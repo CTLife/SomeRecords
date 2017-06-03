d52ab28dce27dfab25c5af187f8184c0db440d61
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2017-06-03 20:39:28 +0800

    Create kallisto-build-index.sh

---
+++ BuildIndex/RNAseq2/kallisto-build-index.sh
@@ -0,0 +1,32 @@
+mkdir		ce11						
+kallisto  index 	--index=ce11/ce11.RefSeq       Shortcuts/ce11/ce11.RefSeq.fasta	        	        >>ce11.buildIndex.runLog	2>&1
+
+mkdir		danRer10						
+kallisto  index 	--index=danRer10/danRer10.RefSeq       Shortcuts/danRer10/danRer10.RefSeq.fasta	        	        >>danRer10.buildIndex.runLog	2>&1
+
+mkdir		dm6						
+kallisto  index 	--index=dm6/dm6.RefSeq       Shortcuts/dm6/dm6.RefSeq.fasta	        	        >>dm6.buildIndex.runLog	2>&1
+
+mkdir		hg38						
+kallisto  index 	--index=hg38/hg38.RefSeq       Shortcuts/hg38/hg38.RefSeq.fasta	        	        >>hg38.buildIndex.runLog	2>&1
+
+mkdir		mm10						
+kallisto  index 	--index=mm10/mm10.RefSeq       Shortcuts/mm10/mm10.RefSeq.fasta	        	        >>mm10.buildIndex.runLog	2>&1
+
+mkdir		sacCer3						
+kallisto  index 	--index=sacCer3/sacCer3.RefSeq       Shortcuts/sacCer3/sacCer3.RefSeq.fasta	        	        >>sacCer3.buildIndex.runLog	2>&1
+
+
+
+
+
+
+mkdir		hg38.ensembl.cDNA						
+kallisto  index 	--index=hg38.ensembl.cDNA/hg38.ensembl.cDNA       Shortcuts/hg38/hg38.ensembl.cDNA.fasta	        	        >>hg38.ensembl.cDNA.buildIndex.runLog	2>&1
+
+mkdir		mm10.ensembl.cDNA						
+kallisto  index 	--index=mm10.ensembl.cDNA/mm10.ensembl.cDNA       Shortcuts/mm10/mm10.ensembl.cDNA.fasta	        	        >>mm10.ensembl.cDNA.buildIndex.runLog	2>&1
+
+
+
+
