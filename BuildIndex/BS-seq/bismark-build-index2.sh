b4cd48e26010ecb7b9937b0ba3944d80bcabd997
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2017-06-03 20:58:28 +0800

    Create bismark-build-index2.sh

---
+++ BuildIndex/BS-seq/bismark-build-index2.sh
@@ -0,0 +1,17 @@
+				
+bismark_genome_preparation 	     --bowtie2 	  --verbose   --genomic_composition      Shortcuts/OtherGenomes/Adapters/	      >> Adapters.buildIndex.runLog	2>&1
+bismark_genome_preparation	     --bowtie2	  --verbose   --genomic_composition      Shortcuts/OtherGenomes/Ecoli/	              >> Ecoli.buildIndex.runLog	2>&1
+bismark_genome_preparation	     --bowtie2	  --verbose   --genomic_composition      Shortcuts/OtherGenomes/phiX174/	      >> phiX174.buildIndex.runLog	2>&1
+bismark_genome_preparation	     --bowtie2	  --verbose   --genomic_composition      Shortcuts/OtherGenomes/UniVectors/	      >> UniVectors.buildIndex.runLog	2>&1
+
+
+bismark_genome_preparation 	     --bowtie2 	  --verbose   --genomic_composition      Shortcuts/ce11/ce11/	              >> ce11.buildIndex.runLog	2>&1
+bismark_genome_preparation 	     --bowtie2 	  --verbose   --genomic_composition      Shortcuts/danRer10/danRer10/	      >> danRer10.buildIndex.runLog	2>&1
+bismark_genome_preparation 	     --bowtie2 	  --verbose   --genomic_composition      Shortcuts/dm6/dm6/	              >> dm6.buildIndex.runLog	2>&1
+bismark_genome_preparation 	     --bowtie2 	  --verbose   --genomic_composition      Shortcuts/hg38/hg38/	              >> hg38.buildIndex.runLog	2>&1
+bismark_genome_preparation 	     --bowtie2 	  --verbose   --genomic_composition      Shortcuts/mm10/mm10/	              >> mm10.buildIndex.runLog	2>&1
+bismark_genome_preparation 	     --bowtie2 	  --verbose   --genomic_composition      Shortcuts/sacCer3/sacCer3/	      >> sacCer3.buildIndex.runLog	2>&1
+
+
+
+
