78a6155e00c5d90120e399a95bdde4aab0b1ba94
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2017-09-28 16:40:34 +0800

    Add files via upload

---
+++ shell/bedgraph-sorted.sh
@@ -0,0 +1,10 @@
+sort -k1,1 -k2,2n	bedgraph/GSE36552_2-cell.bedgraph   >   bedgraph-sorted/GSE36552_2-cell.bedgraph
+sort -k1,1 -k2,2n	bedgraph/GSE36552_4-cell.bedgraph   >      bedgraph-sorted/GSE36552_4-cell.bedgraph
+sort -k1,1 -k2,2n	bedgraph/GSE36552_8-cell.bedgraph   >      bedgraph-sorted/GSE36552_8-cell.bedgraph
+sort -k1,1 -k2,2n	bedgraph/GSE36552_epi.bedgraph   >      bedgraph-sorted/GSE36552_epi.bedgraph
+sort -k1,1 -k2,2n	bedgraph/GSE36552_ESC.bedgraph   >      bedgraph-sorted/GSE36552_ESC.bedgraph
+sort -k1,1 -k2,2n	bedgraph/GSE36552_Morluae.bedgraph   >      bedgraph-sorted/GSE36552_Morluae.bedgraph
+sort -k1,1 -k2,2n	bedgraph/GSE36552_oocyte.bedgraph    >     bedgraph-sorted/GSE36552_oocyte.bedgraph
+sort -k1,1 -k2,2n	bedgraph/GSE36552_PE.bedgraph     >    bedgraph-sorted/GSE36552_PE.bedgraph
+sort -k1,1 -k2,2n	bedgraph/GSE36552_TE.bedgraph     >    bedgraph-sorted/GSE36552_TE.bedgraph
+sort -k1,1 -k2,2n	bedgraph/GSE36552_zygote.bedgraph    >     bedgraph-sorted/GSE36552_zygote.bedgraph
