bb935a03eb3e97301842eb7e2646a15cc59d3c59
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2017-11-29 22:28:41 +0800

    Add files via upload

---
+++ RRBS-PCA/clusterONE_FemaleTwins-Parents-4.sh
@@ -0,0 +1,11 @@
+Rscript  clusterONE_FemaleTwins-Parents-4.R     11B_splitXY/100A-rmXY/0_cov5reads    1011B_splitXY/100A-rmXY/0_cov5reads/clusterONE_FemaleTwins-Parents-4/NC-vs-ICSIfrozen          
+Rscript  clusterONE_FemaleTwins-Parents-4.R     11B_splitXY/100A-rmXY/1_cov10reads   1011B_splitXY/100A-rmXY/1_cov10reads/clusterONE_FemaleTwins-Parents-4/NC-vs-ICSIfrozen          
+Rscript  clusterONE_FemaleTwins-Parents-4.R     11B_splitXY/100A-rmXY/2_cov20reads   1011B_splitXY/100A-rmXY/2_cov20reads/clusterONE_FemaleTwins-Parents-4/NC-vs-ICSIfrozen          
+Rscript  clusterONE_FemaleTwins-Parents-4.R     11B_splitXY/100A-rmXY/3_cov30reads   1011B_splitXY/100A-rmXY/3_cov30reads/clusterONE_FemaleTwins-Parents-4/NC-vs-ICSIfrozen          
+Rscript  clusterONE_FemaleTwins-Parents-4.R     11B_splitXY/100A-rmXY/4_cov40reads   1011B_splitXY/100A-rmXY/4_cov40reads/clusterONE_FemaleTwins-Parents-4/NC-vs-ICSIfrozen          
+Rscript  clusterONE_FemaleTwins-Parents-4.R     11B_splitXY/100A-rmXY/5_cov50reads   1011B_splitXY/100A-rmXY/5_cov50reads/clusterONE_FemaleTwins-Parents-4/NC-vs-ICSIfrozen          
+
+
+ 
+
+
