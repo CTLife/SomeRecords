fd000110b2878736e70044fc37ac343be22b591b
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2017-12-12 18:42:57 +0800

    Add files via upload

---
+++ forRRBS/MeLevelAnalysis/RRBS2_MaleFemaleTwins-2.sh
@@ -0,0 +1,34 @@
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100A-rmXY/0_cov5reads    2011B_splitXY/100A-rmXY/0_cov5reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100A-rmXY/1_cov10reads   2011B_splitXY/100A-rmXY/1_cov10reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100A-rmXY/2_cov20reads   2011B_splitXY/100A-rmXY/2_cov20reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100A-rmXY/3_cov30reads   2011B_splitXY/100A-rmXY/3_cov30reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     1B_splitXY/100A-rmXY                 2001B_splitXY/100A-rmXY/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+
+
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100C-onlyX/0_cov5reads    2011B_splitXY/100C-onlyX/0_cov5reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100C-onlyX/1_cov10reads   2011B_splitXY/100C-onlyX/1_cov10reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100C-onlyX/2_cov20reads   2011B_splitXY/100C-onlyX/2_cov20reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100C-onlyX/3_cov30reads   2011B_splitXY/100C-onlyX/3_cov30reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     1B_splitXY/100C-onlyX                 2001B_splitXY/100C-onlyX/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+
+
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100D-onlyY/0_cov5reads    2011B_splitXY/100D-onlyY/0_cov5reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100D-onlyY/1_cov10reads   2011B_splitXY/100D-onlyY/1_cov10reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100D-onlyY/2_cov20reads   2011B_splitXY/100D-onlyY/2_cov20reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11B_splitXY/100D-onlyY/3_cov30reads   2011B_splitXY/100D-onlyY/3_cov30reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     1B_splitXY/100D-onlyY                 2001B_splitXY/100D-onlyY/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+
+
+ 
+
+
+Rscript  RRBS2_MaleFemaleTwins-2.R     11A_All-Chromosomes/0_cov5reads    2011A_All-Chromosomes/0_cov5reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11A_All-Chromosomes/1_cov10reads   2011A_All-Chromosomes/1_cov10reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11A_All-Chromosomes/2_cov20reads   2011A_All-Chromosomes/2_cov20reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     11A_All-Chromosomes/3_cov30reads   2011A_All-Chromosomes/3_cov30reads/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+Rscript  RRBS2_MaleFemaleTwins-2.R     1A_All-Chromosomes                 2001A_All-Chromosomes/RRBS2_MaleFemaleTwins-2/NC_vs_IVF-frozen          
+ 
+
+
+
+
