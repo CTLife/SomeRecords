a7ac0b25ab005cb6dfcee235d10100486dd7e51b
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2020-04-25 12:49:55 -0500

    Update scale.R

--- ForHeatmap/scale.R
+++ ForHeatmap/scale.R
@@ -1,5 +1,5 @@
 my_reset_outliers <- function(x, na.rm = TRUE ) { ## x is a numerical vector or matrix.
-  qnt <- quantile(x, probs=c(0.01, 0.99) , type=1,  na.rm = na.rm )  
+  qnt <- quantile(x, probs=c(0.05, 0.95) , type=1,  na.rm = na.rm )  
   y <- x
   y[x < qnt[1] ] <- qnt[1]
   y[x > qnt[2] ] <- qnt[2]    
